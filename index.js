9b7ac6baee354171db8a5864a4f5116163969e70
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-10-13 15:56:26 -0400

    first init lets get it

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const projectname = "Social Network";
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${projectname} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
